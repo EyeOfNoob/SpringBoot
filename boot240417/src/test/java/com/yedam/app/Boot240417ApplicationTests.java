4f26715ff2737dc6266f9cfdd93d9a7ac43d1975
EyeOfNoob <crescentbear@naver.com>
2024-05-08 15:01:54 +0900

    UPDATE

--- boot240417/src/test/java/com/yedam/app/Boot240417ApplicationTests.java
+++ boot240417/src/test/java/com/yedam/app/Boot240417ApplicationTests.java
@@ -2,6 +2,7 @@ package com.yedam.app;
 
 import java.util.Date;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,7 +30,7 @@ class Boot240417ApplicationTests {
 	@Autowired
 	PasswordEncoder passwordEncoder;
 	
-	@Test
+	//@Test
 	public void testEncoder() {
 		String password = "1234";
 		
@@ -38,6 +39,24 @@ class Boot240417ApplicationTests {
 //													  원래 데이터, 암호화된 데이터
 		boolean matchResult = passwordEncoder.matches(password, enPwd);
 		System.out.println("matchResult : " + matchResult);
+	}
+	
+	@Autowired
+	StringEncryptor jasyptStringEncryptor;
+	
+	@Test
+	public void encryption() {
+		String[] strs = {
+				"net.sf.log4jdbc.sql.jdbcapi.DriverSpy",
+				"jdbc:log4jdbc:oracle:thin:@127.0.0.1:1521:xe",
+				"hr",
+				"hr"
+		};
+		
+		for(String str : strs) {
+			String encStr = jasyptStringEncryptor.encrypt(str);
+			System.out.println(encStr);
+		}
 	}
 	
 }
